23985b391d4c3e80eecaf7faf896fb0e7cda5f75
Sophia Hunt <sophiahunt2001@icloud.com>
2024-10-30 18:35:06 -0400

    added pixelated rounded corners to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,21 +4,26 @@ import * as navbarStyles from './navbar.module.css';
 
 const Navbar = () => {
     return (
-        <nav className={navbarStyles.navbar}>
-            <div>
-                <Link to='/' className={navbarStyles.logo}>Sophia Hunt</Link>
-            </div>
-            <div>
-                <ul className={navbarStyles.list}>
-                    <li>
-                        <Link to ='../about' className={navbarStyles.links}>about</Link>
-                    </li>
-                    <li>
-                        <Link to='../portfolio'  className={navbarStyles.links}>portfolio</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <div className={navbarStyles.pixelCorners}>
+            <nav className={navbarStyles.navbar}>
+                <div>
+                    <Link to='/' className={navbarStyles.logo}>home</Link>
+                </div>
+                <div>
+                    <ul className={navbarStyles.list}>
+                        <li>
+                            <Link to ='../about' className={navbarStyles.links}>about</Link>
+                        </li>
+                        <li>
+                            <Link to='../portfolio'  className={navbarStyles.links}>portfolio</Link>
+                        </li>
+                        <li>
+                            <Link to='../contact'  className={navbarStyles.links}>contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     )
 }
 
