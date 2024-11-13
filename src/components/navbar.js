497313049d0b941c1e18e29f076bc52c603f2cad
Sophia Hunt <sophiahunt2001@icloud.com>
2024-11-13 16:25:36 -0500

    complete redesign of navbar, working on making it mobile friendly

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,25 +1,42 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { Link } from 'gatsby';
 import * as navbarStyles from './navbar.module.css';
+import logo from '../images/logo.png';
+import hamburger from "../icons/hamburger.png";
 
 const Navbar = () => {
+    const [showNavbar, setShowNavbar] = useState(false);
+
+    const handleShowNavbar = () => {
+        setShowNavbar(!showNavbar);
+    }
+
     return (
             <nav className={navbarStyles.navbar}>
-                <div>
-                    <Link to='/' className={navbarStyles.home}>home</Link>
-                </div>
-                <div>
-                    <ul className={navbarStyles.list}>
-                        <li>
-                            <Link to ='../about' className={navbarStyles.links}>about</Link>
-                        </li>
-                        <li>
-                            <Link to='../work'  className={navbarStyles.links}>work</Link>
-                        </li>
-                        <li>
-                            <Link to='../contact'  className={navbarStyles.links}>contact</Link>
-                        </li>
-                    </ul>
+                <div className={navbarStyles.container}>
+                    <div className={navbarStyles.logo}>
+                        <img src={logo} />
+                    </div>
+                    <div className={navbarStyles.menuIcon} onClick={handleShowNavbar}>
+                        <img src={hamburger} />
+                    </div>
+                    <div className={navbarStyles.navElements}>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="../about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="../work">Work</Link>
+                            </li>
+                            <li>
+                            <Link to="../contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
     )
