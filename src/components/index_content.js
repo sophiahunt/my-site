5cb656a081bee869ec2b450544a0dbf3c940d0c9
Sophia Hunt <sophiahunt2001@icloud.com>
2024-11-14 12:43:12 -0500

    working on adding background image behind text on landing page

--- src/components/index_content.js
+++ src/components/index_content.js
@@ -1,19 +1,22 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 import * as contentStyles from './index_content.module.css';
-import sophia from "../images/sophia-alt.png"
+import sophia from "../images/sophia-alt.png";
+import retrowave from "../images/retrowave.png";
 
 const IndexContent = () => {
     return (
-        <div className={contentStyles.container}>
-            <div className={contentStyles.leftContainer}>
-                <img src={sophia} alt="Sophia Hunt" />
-            </div>
-   
-            <div className={contentStyles.rightContainer}>
-                <p className={contentStyles.header}>HELLO! MY NAME IS</p>
-                <p>Sophia Hunt</p>
-                <p className={contentStyles.header}>SOFTWARE DEVELOPER AND DESIGNER</p>
+        <div className={contentStyles.retroWave}>
+            <div className={contentStyles.container}>
+                <div className={contentStyles.leftContainer}>
+                    <img src={sophia} alt="Sophia Hunt" />
+                </div>
+    
+                <div className={contentStyles.rightContainer}>
+                    <p className={contentStyles.header}>HELLO! MY NAME IS</p>
+                    <p>Sophia Hunt</p>
+                    <p className={contentStyles.header}>SOFTWARE DEVELOPER AND DESIGNER</p>
+                </div>
             </div>
         </div>
     )
