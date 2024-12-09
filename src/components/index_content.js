eb2b9a1731e5aac063fb296ac413f6277b6b94da
Sophia Hunt <sophiahunt2001@icloud.com>
2024-12-09 13:28:24 -0500

    changed color scheme and logo yet again

--- src/components/index_content.js
+++ src/components/index_content.js
@@ -2,21 +2,20 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 import * as contentStyles from './index_content.module.css';
 import sophia from "../images/sophia-alt.png";
-import retrowave from "../images/retrowave.png";
+import logo from '../images/logo.png';
 
 const IndexContent = () => {
     return (
-        <div className={contentStyles.retroWave}>
-            <div className={contentStyles.container}>
-                <div className={contentStyles.leftContainer}>
-                    <img src={sophia} alt="Sophia Hunt" />
-                </div>
+        <div className={contentStyles.container}>
+            <div className={contentStyles.leftContainer}>
+                <img src={sophia} alt="Sophia Hunt" />
+            </div>
     
-                <div className={contentStyles.rightContainer}>
-                    <p className={contentStyles.header}>HELLO! MY NAME IS</p>
-                    <p>Sophia Hunt</p>
-                    <p className={contentStyles.header}>SOFTWARE DEVELOPER AND DESIGNER</p>
-                </div>
+            <div className={contentStyles.rightContainer}>
+                <img src={logo} />
+                <p className={contentStyles.header}>HELLO! MY NAME IS</p>
+                <p>Sophia Hunt</p>
+                <p className={contentStyles.header}>SOFTWARE DEVELOPER AND DESIGNER</p>
             </div>
         </div>
     )
