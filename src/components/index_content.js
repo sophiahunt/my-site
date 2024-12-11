63b277646a15272ccb65b6c61b16c9c79094db64
Sophia Hunt <sophiahunt2001@icloud.com>
2024-12-10 22:12:29 -0500

    final logo edit and changed images on landing page

--- src/components/index_content.js
+++ src/components/index_content.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 import * as contentStyles from './index_content.module.css';
-import sophia from "../images/sophia-alt.png";
-import logo from '../images/logo.png';
+import sophia from "../images/soop1.png";
+import sophia2 from '../images/soop2.png';
 
 const IndexContent = () => {
     return (
@@ -12,10 +12,10 @@ const IndexContent = () => {
             </div>
     
             <div className={contentStyles.rightContainer}>
-                <img src={logo} />
                 <p className={contentStyles.header}>HELLO! MY NAME IS</p>
                 <p>Sophia Hunt</p>
                 <p className={contentStyles.header}>SOFTWARE DEVELOPER AND DESIGNER</p>
+                <img src={sophia2} />
             </div>
         </div>
     )
