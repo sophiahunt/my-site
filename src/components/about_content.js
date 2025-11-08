315a3fb0912d547f5cc6c4d6148e818485913ed4
Sophia Hunt <sophiahunt2001@icloud.com>
2025-11-08 18:59:27 -0500

    set up flexbox and main images on about page

--- src/components/about_content.js
+++ src/components/about_content.js
@@ -2,12 +2,21 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 import Navbar from './navbar';
 import * as contentStyles from './about_content.module.css';
+import mainImage from "../images/about_main.png";
+import paper from '../images/paper.png';
 
 const AboutContent = ({ pageTitle }) => {
     return (
-        <div className={contentStyles.container}>
+        <div className={contentStyles.main}>
             <Navbar />
-            <p>hey guys</p>
+            <div className={contentStyles.container}>
+                <div className={contentStyles.leftContainer}>
+                    <img src={paper} />
+                </div>
+                <div className={contentStyles.rightContainer}>
+                    <img src={mainImage}/>
+                </div>
+            </div>
         </div>
     )
 }
